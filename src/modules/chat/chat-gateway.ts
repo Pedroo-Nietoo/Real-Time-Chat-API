6da5a2df5d47bb86d465fd17a9fa1ee34c725b0e
Pedro Nieto <pedronieto.2005@gmail.com>
2024-09-19 13:08:06 -0300

    feat: Add complete chat system
    This commit adds the full chat system, allowing the user to send private messages, join, message and leave groups, as well as check the user's status - whether online, offline or away

--- src/modules/chat/chat-gateway.ts
+++ src/modules/chat/chat-gateway.ts
@@ -2,11 +2,13 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(3031, { cors: { origin: '*' } })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
   handleConnection(client: Socket) {
@@ -21,8 +23,44 @@ export class ChatGateway {
     });
   }
 
-  @SubscribeMessage('message')
-  handleMessage(client: Socket, message: any) {
-    client.broadcast.emit('message', message);
+  @SubscribeMessage('private-message')
+  handlePrivateMessage(
+    client: Socket,
+    payload: { receiverId: string; message: string },
+  ) {
+    const { receiverId, message } = payload;
+    this.server.to(receiverId).emit('private-message', {
+      senderId: client.id,
+      message,
+    });
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(client: Socket, room: string) {
+    client.join(room['data']);
+    client.to(room['data']).emit('user-joined', {
+      message: `${client.id} joined room ${room['data']}`,
+    });
+  }
+
+  @SubscribeMessage('group-message')
+  handleGroupMessage(
+    client: Socket,
+    payload: { room: string; message: string },
+  ) {
+    const { room, message } = payload;
+
+    client.to(room).emit('group-message', {
+      senderId: client.id,
+      message,
+    });
+  }
+
+  @SubscribeMessage('leave-room')
+  handleLeaveRoom(client: Socket, room: string) {
+    client.leave(room['data']);
+    client.to(room['data']).emit('user-left', {
+      message: `${client.id} left room ${room['data']}`,
+    });
   }
 }
